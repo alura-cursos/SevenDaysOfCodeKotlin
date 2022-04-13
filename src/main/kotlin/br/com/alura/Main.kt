c357484d0d25ba568f6c751973a5888baf3563c9
Alex Felipe <alexfelipevieira@gmail.com>
2022-04-13 10:11:19 -0300

    Day 4

--- src/main/kotlin/br/com/alura/Main.kt
+++ src/main/kotlin/br/com/alura/Main.kt
@@ -2,7 +2,10 @@ package br.com.alura
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -12,6 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -19,74 +23,105 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import br.com.alura.extensions.loadImageBitmap
+import br.com.alura.model.Movie
 
 @Composable
 @Preview
 fun App() {
-    val image =
-        "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg"
     MaterialTheme(
         colors = darkColors()
     ) {
         Surface {
             Box(modifier = Modifier.fillMaxSize()) {
-                Column(
-                    modifier = Modifier
-                        .width(200.dp)
-                        .padding(16.dp)
-                ) {
-                    Image(
-                        bitmap = image.loadImageBitmap(),
-                        contentDescription = "capa do filme",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clip(RoundedCornerShape(4.dp))
+                val movies = listOf(
+                    Movie(
+                        title = "The Shawshank Redemption",
+                        image = "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                        rating = 9.2,
+                        year = 1994
+                    ),
+                    Movie(
+                        title = "The Godfather",
+                        image = "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX128_CR0,1,128,176_AL_.jpg",
+                        rating = 9.2,
+                        year = 1972
+                    ),
+                    Movie(
+                        title = "The Dark Knight",
+                        image = "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX128_CR0,3,128,176_AL_.jpg",
+                        rating = 9.0,
+                        year = 2008
                     )
-                    Row(
-                        modifier = Modifier.fillMaxWidth()
-                            .padding(
-                                top = 8.dp,
-                                start = 8.dp,
-                                end = 8.dp
-                            ),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            Icon(
-                                Icons.Default.Star,
-                                "ícone de estrela para nota",
-                                tint = Color.Yellow,
-                                modifier = Modifier.height(16.dp)
-                            )
-                            Text(
-                                "9.2",
-                                modifier = Modifier
-                                    .padding(start = 2.dp),
-                                color = Color(0xffeeeeee),
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight.Bold
-                            )
-                        }
-                        Text(
-                            "1994",
-                            fontSize = 14.sp,
-                            color = Color(0xffeeeeee),
+                )
+                LazyColumn {
+                    items(movies) { movie ->
+                        MovieItem(
+                            movie
                         )
                     }
-                    Text(
-                        "The Shawshank Redemption",
-                        modifier = Modifier.padding(
-                            start = 16.dp,
-                            top = 8.dp,
-                            end = 16.dp
-                        ),
-                        fontSize = 12.sp,
-                        textAlign = TextAlign.Center
-                    )
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun MovieItem(movie: Movie) {
+    Column(
+        modifier = Modifier
+            .width(200.dp)
+            .padding(16.dp)
+    ) {
+        Image(
+            bitmap = movie.image.loadImageBitmap(),
+            contentDescription = "capa do filme",
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(
+                    top = 8.dp,
+                    start = 8.dp,
+                    end = 8.dp
+                ),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    Icons.Default.Star,
+                    "ícone de estrela para nota",
+                    tint = Color.Yellow,
+                    modifier = Modifier.height(16.dp)
+                )
+                Text(
+                    movie.rating.toString(),
+                    modifier = Modifier
+                        .padding(start = 2.dp),
+                    color = Color(0xffeeeeee),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+            Text(
+                movie.year.toString(),
+                fontSize = 14.sp,
+                color = Color(0xffeeeeee),
+            )
+        }
+        Text(
+            movie.title,
+            modifier = Modifier.padding(
+                start = 16.dp,
+                top = 8.dp,
+                end = 16.dp
+            ),
+            fontSize = 12.sp,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
---
+++ src/main/kotlin/br/com/alura/model/Movie.kt
@@ -0,0 +1,8 @@
+package br.com.alura.model
+
+data class Movie(
+    val title: String,
+    val image: String,
+    val rating: Double,
+    val year: Int
+)
