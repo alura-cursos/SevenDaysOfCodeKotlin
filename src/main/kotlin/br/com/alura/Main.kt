21186acc8b06e901c5910ebc37c70f8fac8089af
Alex Felipe <alexfelipevieira@gmail.com>
2022-04-13 08:35:51 -0300

    Day 1

--- src/main/kotlin/br/com/alura/Main.kt
+++ src/main/kotlin/br/com/alura/Main.kt
@@ -1,33 +1,39 @@
 package br.com.alura
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Text("The Shawshank Redemption")
+            Image(
+                painter = painterResource("capa-filme.jpg"),
+                contentDescription = "capa do filme",
+                modifier = Modifier.height(200.dp)
+            )
+            Text("Nota: 9.2 - Ano: 1994")
         }
     }
 }
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "IMDB"
+    ) {
         App()
     }
 }
