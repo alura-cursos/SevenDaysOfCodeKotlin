884b58410036285d1ca37b146f267790d4bb60a2
Alex Felipe <alexfelipevieira@gmail.com>
2022-04-13 08:44:57 -0300

    Day 3

--- src/main/kotlin/br/com/alura/Main.kt
+++ src/main/kotlin/br/com/alura/Main.kt
@@ -2,13 +2,20 @@ package br.com.alura
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import br.com.alura.extensions.loadImageBitmap
@@ -18,15 +25,67 @@ import br.com.alura.extensions.loadImageBitmap
 fun App() {
     val image =
         "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX674_.jpg"
-    MaterialTheme {
-        Column {
-            Text("The Shawshank Redemption")
-            Image(
-                bitmap = image.loadImageBitmap(),
-                contentDescription = "capa do filme",
-                modifier = Modifier.height(200.dp)
-            )
-            Text("Nota: 9.2 - Ano: 1994")
+    MaterialTheme(
+        colors = darkColors()
+    ) {
+        Surface {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .width(200.dp)
+                        .padding(16.dp)
+                ) {
+                    Image(
+                        bitmap = image.loadImageBitmap(),
+                        contentDescription = "capa do filme",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(4.dp))
+                    )
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(
+                                top = 8.dp,
+                                start = 8.dp,
+                                end = 8.dp
+                            ),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                Icons.Default.Star,
+                                "ícone de estrela para nota",
+                                tint = Color.Yellow,
+                                modifier = Modifier.height(16.dp)
+                            )
+                            Text(
+                                "9.2",
+                                modifier = Modifier
+                                    .padding(start = 2.dp),
+                                color = Color(0xffeeeeee),
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Text(
+                            "1994",
+                            fontSize = 14.sp,
+                            color = Color(0xffeeeeee),
+                        )
+                    }
+                    Text(
+                        "The Shawshank Redemption",
+                        modifier = Modifier.padding(
+                            start = 16.dp,
+                            top = 8.dp,
+                            end = 16.dp
+                        ),
+                        fontSize = 12.sp,
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
         }
     }
 }
